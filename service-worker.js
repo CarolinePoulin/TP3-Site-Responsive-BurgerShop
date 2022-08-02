9f67b78dd00f3bb466a9767b62f9f60e46cc507a
MaxineCP <caropoulin123@hotmail.com>
2022-08-02 14:44:43 -0400

    fetch

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1';
+const CACHE_NAME = 'static-cache-v2';
 
 // Add list of files to cache here.
 const FILES_TO_CACHE = [
@@ -27,6 +27,15 @@ self.addEventListener('install', (evt) => {
 self.addEventListener('activate', (evt) => {
     console.log('[ServiceWorker]Activate');
     // Remove previous cached data from disk.
+    evt.waitUntil(
+        caches.keys().then((keylist) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        }));
 
     self.clients.claim();
 });
@@ -34,4 +43,18 @@ self.addEventListener('activate', (evt) => {
 self.addEventListener('fetch', (evt) => {
     console.log('[ServiceWorker]Fetch', evt.request.url);
     // Add fetch event handler here.
+    if(evt.request.mode !== 'navigate') {
+        // Not a page navigation, bail.
+
+        return;
+    }
+
+    evt.respondWith(
+        fetch(evt.request)
+        .catch(() => {
+            return caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.match('offline.html');
+            });
+        }));
 });
