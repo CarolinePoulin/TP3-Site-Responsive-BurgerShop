a331b44696574c6ae8831cdc95b16255ce7ca343
MaxineCP <caropoulin123@hotmail.com>
2022-08-02 18:24:19 -0400

    cache 7

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v6';
+const CACHE_NAME = 'static-cache-v7';
 
 // Add list of files to cache here.
 const FILES_TO_CACHE = [
